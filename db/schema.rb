a85bf6ccff8b6d24e3dc076574e23e6ec430c703
Carlos Quiroga <cquiro@gmail.com>
2015-07-29 21:24:16 -0400

    Add Company Name column to suppliers, add fields to Supplier Sign Up form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730001914) do
+ActiveRecord::Schema.define(version: 20150730011116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150730001914) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "company_name"
   end
 
   add_index "suppliers", ["username"], name: "index_suppliers_on_username", unique: true, using: :btree
