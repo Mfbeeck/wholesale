da3e047a2127d817c09f4a002275deb464ec14da
Carlos Quiroga <cquiro@gmail.com>
2015-08-03 15:43:06 -0400

    Scope for unique customer and deal id on order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,22 @@
 class Order < ActiveRecord::Base
   belongs_to :deal
   belongs_to :consumer
+
+  after_save :check_if_threshold_reached
+
+  validates :consumer_id, uniqueness: { scope: :deal_id,
+    message: "can only have one bid per deal." }
+
+
+
+    private
+
+  def check_if_threshold_reached
+    if deal.orders.count >= deal.threshold
+
+    else
+
+    end
+  end
+
 end
