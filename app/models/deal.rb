b817cbd323aa33ea0f94d1d73e49bc199ae55858
Mfbeeck <mfbeeck@gmail.com>
2015-08-03 17:06:46 -0400

    threshold logic

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,4 +1,15 @@
 class Deal < ActiveRecord::Base
   belongs_to :supplier
   has_many :orders
+
+
+   def is_threshold_reached
+    if orders.count >= threshold
+    	true
+    else
+    	false
+    end
+  end
+
+  private
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,21 +2,14 @@ class Order < ActiveRecord::Base
   belongs_to :deal
   belongs_to :consumer
 
-  after_save :check_if_threshold_reached
+  # after_save :check_if_threshold_reached
 
   validates :consumer_id, uniqueness: { scope: :deal_id,
     message: "can only have one bid per deal." }
 
 
 
-    private
+  private
 
-  def check_if_threshold_reached
-    if deal.orders.count >= deal.threshold
-
-    else
-
-    end
-  end
 
 end
