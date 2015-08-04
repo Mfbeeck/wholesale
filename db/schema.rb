b29a0425df0d40f242f41567ea44dc45f24ddfed
Mfbeeck <mfbeeck@gmail.com>
2015-08-04 12:37:22 -0400

    added several security features and notices

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804040252) do
+ActiveRecord::Schema.define(version: 20150804151318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20150804040252) do
     t.integer  "quantity"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "Address"
+    t.string   "address"
   end
 
   add_index "orders", ["consumer_id"], name: "index_orders_on_consumer_id", using: :btree
