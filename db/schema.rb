fa199aa03b02f4d27010e62ecb25c2e9c7530a22
Carlos Quiroga <cquiro@gmail.com>
2015-08-04 22:07:52 -0400

    Home styling. White and black

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20150805002636) do
     t.integer  "quantity"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "Address"
     t.string   "address"
   end
 
